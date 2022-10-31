21051d4a4f0ddc0eea015d06e9c26273403b42fc
wangjie <89757316@qq.com>
2022-10-31 22:57:00 +0800

    ...

--- src/test/java/PoiExcelTest.java
+++ src/test/java/PoiExcelTest.java
@@ -1,35 +1,33 @@
-//import com.wj.core.tools.excel.ApachePoiExcelUtil;
-//import lombok.Data;
-//import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-//import org.junit.Test;
-//
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class PoiExcelTest {
-//
-//    @Test
-//    public static void main(String[] args) throws SQLException {
-//        List<Student> list = new ArrayList<>();
-//        Student s1 = new Student();
-//        s1.setName("wangjie");
-//        s1.setCourse("math");
-//        s1.setScore(100);
-//        Student s2 = new Student();
-//        s2.setName("wangjun");
-//        s2.setCourse("math");
-//        s2.setScore(90);
-//        list.add(s1);
-//        list.add(s2);
-//        final String title[] = {"名字:name:50", "课程:course:50", "分数:score:50"};
-//        final String sheetName = "sheet1";
-//        HSSFWorkbook workbook = ApachePoiExcelUtil.export(null,list, title, sheetName);
-//    }
-//    @Data
-//    static class Student {
-//        private String name;
-//        private String course;
-//        private int score;
-//    }
-//}
+import io.github.wj0410.core.tools.excel.ApachePoiExcelUtil;
+import lombok.Data;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoiExcelTest {
+
+    public static void main(String[] args) throws SQLException {
+        List<Student> list = new ArrayList<>();
+        Student s1 = new Student();
+        s1.setName("wangjie");
+        s1.setCourse("math");
+        s1.setScore(100);
+        Student s2 = new Student();
+        s2.setName("wangjun");
+        s2.setCourse("math");
+        s2.setScore(90);
+        list.add(s1);
+        list.add(s2);
+        final String title[] = {"名字:name:50", "课程:course:50", "分数:score:50"};
+        final String sheetName = "sheet1";
+        HSSFWorkbook workbook = ApachePoiExcelUtil.export(null,list, title, sheetName);
+    }
+    @Data
+    static class Student {
+        private String name;
+        private String course;
+        private int score;
+    }
+}
--- src/test/java/TimeOverlapTest.java
+++ src/test/java/TimeOverlapTest.java
@@ -1,34 +1,32 @@
-//import com.wj.core.tools.util.TimeOverlapUtil;
-//import org.apache.commons.lang3.time.DateUtils;
-//import org.junit.Test;
-//
-//import java.text.ParseException;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//
-//public class TimeOverlapTest {
-//    @Test
-//    public static void main(String[] args) throws ParseException {
-//        String d1s = "2022-01-01 10:00:00";
-//        String d1e = "2022-01-01 18:00:00";
-//        String d2s = "2022-01-02 10:00:00";
-//        String d2e = "2022-01-02 18:00:00";
-//        String d3s = "2022-01-03 10:00:00";
-//        String d3e = "2022-01-03 18:00:00";
-//        Date date1s = DateUtils.parseDate(d1s, "yyyy-MM-dd HH:mm:ss");
-//        Date date1e = DateUtils.parseDate(d1e, "yyyy-MM-dd HH:mm:ss");
-//        Date date2s = DateUtils.parseDate(d2s, "yyyy-MM-dd HH:mm:ss");
-//        Date date2e = DateUtils.parseDate(d2e, "yyyy-MM-dd HH:mm:ss");
-//        Date date3s = DateUtils.parseDate(d3s, "yyyy-MM-dd HH:mm:ss");
-//        Date date3e = DateUtils.parseDate(d3e, "yyyy-MM-dd HH:mm:ss");
-//        List<TimeOverlapUtil.TimePair> timePairList = new ArrayList<>();
-//        TimeOverlapUtil.TimePair t1 = new TimeOverlapUtil.TimePair(date1s.getTime(), date1e.getTime());
-//        TimeOverlapUtil.TimePair t2 = new TimeOverlapUtil.TimePair(date2s.getTime(), date2e.getTime());
-//        TimeOverlapUtil.TimePair t3 = new TimeOverlapUtil.TimePair(date3s.getTime(), date3e.getTime());
-//        timePairList.add(t1);
-//        timePairList.add(t2);
-//        timePairList.add(t3);
-//        System.out.println(TimeOverlapUtil.isOverlap(timePairList, true));
-//    }
-//}
+import io.github.wj0410.core.tools.util.TimeOverlapUtil;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TimeOverlapTest {
+    public static void main(String[] args) throws ParseException {
+        String d1s = "2022-01-01 10:00:00";
+        String d1e = "2022-01-01 18:00:00";
+        String d2s = "2022-01-02 10:00:00";
+        String d2e = "2022-01-02 18:00:00";
+        String d3s = "2022-01-03 10:00:00";
+        String d3e = "2022-01-03 18:00:00";
+        Date date1s = DateUtils.parseDate(d1s, "yyyy-MM-dd HH:mm:ss");
+        Date date1e = DateUtils.parseDate(d1e, "yyyy-MM-dd HH:mm:ss");
+        Date date2s = DateUtils.parseDate(d2s, "yyyy-MM-dd HH:mm:ss");
+        Date date2e = DateUtils.parseDate(d2e, "yyyy-MM-dd HH:mm:ss");
+        Date date3s = DateUtils.parseDate(d3s, "yyyy-MM-dd HH:mm:ss");
+        Date date3e = DateUtils.parseDate(d3e, "yyyy-MM-dd HH:mm:ss");
+        List<TimeOverlapUtil.TimePair> timePairList = new ArrayList<>();
+        TimeOverlapUtil.TimePair t1 = new TimeOverlapUtil.TimePair(date1s.getTime(), date1e.getTime());
+        TimeOverlapUtil.TimePair t2 = new TimeOverlapUtil.TimePair(date2s.getTime(), date2e.getTime());
+        TimeOverlapUtil.TimePair t3 = new TimeOverlapUtil.TimePair(date3s.getTime(), date3e.getTime());
+        timePairList.add(t1);
+        timePairList.add(t2);
+        timePairList.add(t3);
+        System.out.println(TimeOverlapUtil.isOverlap(timePairList, true));
+    }
+}
