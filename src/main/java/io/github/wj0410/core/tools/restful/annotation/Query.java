95707089096c9853bc4f1bf8b23f0782ad6d704f
wangjie <89757316@qq.com>
2022-11-01 14:45:24 +0800

    Query注解支持多个keyword查询

--- src/main/java/io/github/wj0410/core/tools/restful/annotation/Query.java
+++ src/main/java/io/github/wj0410/core/tools/restful/annotation/Query.java
@@ -14,7 +14,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Query {
-    Keyword value() default Keyword.eq;
+    Keyword[] value() default Keyword.eq;
 
     String column() default "";
 }
--- src/main/java/io/github/wj0410/core/tools/restful/util/QueryUtil.java
+++ src/main/java/io/github/wj0410/core/tools/restful/util/QueryUtil.java
@@ -22,24 +22,14 @@ import java.util.regex.Pattern;
  * date 2021年03月31日14时51分
  */
 public class QueryUtil {
-    //不需要值
-    static List<Keyword> notNeedParam = new ArrayList<>();
-
-    static {
-        notNeedParam.add(Keyword.is_null);
-        notNeedParam.add(Keyword.is_not_null);
-        notNeedParam.add(Keyword.order_asc);
-        notNeedParam.add(Keyword.order_desc);
-    }
-
     private static Pattern linePattern = Pattern.compile("_(\\w)");
     private static Pattern humpPattern = Pattern.compile("[A-Z]");
 
-    public static QueryWrapper buildWrapper(Object query) {
-        if (query == null) {
-            throw new ServiceException("query cannot be null!");
+    public static QueryWrapper buildWrapper(Object queryObj) {
+        if (queryObj == null) {
+            throw new ServiceException("queryObj cannot be null!");
         }
-        Class<?> clazz = query.getClass();
+        Class<?> clazz = queryObj.getClass();
         //获取类所有属性
         List<Field> fields = getFields(clazz, new ArrayList<>());
         QueryWrapper queryWrapper = new QueryWrapper();
@@ -48,55 +38,75 @@ public class QueryUtil {
             Query annotation = field.getAnnotation(Query.class);
             try {
                 field.setAccessible(true);//设置属性可以访问
-                if (annotation != null && (field.get(query) != null
-                        || notNeedParam.contains(annotation.value()))) {
+                if (annotation != null) {
                     //获取注解配置的操作类型值
-                    Keyword keyword = annotation.value();
-                    String column = annotation.column();
-                    if (StringUtils.isBlank(column)) {
-                        column = humpToLine(field.getName());
+                    Keyword[] keywords = annotation.value();
+                    for (Keyword keyword : keywords) {
+                        String column = annotation.column();
+                        if (StringUtils.isBlank(column)) {
+                            column = humpToLine(field.getName());
+                        }
+                        switch (keyword) {
+                            case eq:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.eq(column, field.get(queryObj));
+                                }
+                                break;
+                            case like:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.like(column, field.get(queryObj));
+                                }
+                                break;
+                            case left_like:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.likeLeft(column, field.get(queryObj));
+                                }
+                                break;
+                            case right_like:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.likeRight(column, field.get(queryObj));
+                                }
+                                break;
+                            case gt:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.gt(column, field.get(queryObj));
+                                }
+                                break;
+                            case lt:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.lt(column, field.get(queryObj));
+                                }
+                                break;
+                            case ge:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.ge(column, field.get(queryObj));
+                                }
+                                break;
+                            case le:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.le(column, field.get(queryObj));
+                                }
+                                break;
+                            case not_in:
+                                if (field.get(queryObj) != null) {
+                                    queryWrapper.notIn(column, field.get(queryObj));
+                                }
+                                break;
+                            case is_null:
+                                queryWrapper.isNull(column);
+                                break;
+                            case is_not_null:
+                                queryWrapper.isNotNull(column);
+                                break;
+                            case order_asc:
+                                queryWrapper.orderByAsc(column);
+                                break;
+                            case order_desc:
+                                queryWrapper.orderByDesc(column);
+                                break;
+                        }
                     }
-                    switch (keyword) {
-                        case eq:
-                            queryWrapper.eq(column, field.get(query));
-                            break;
-                        case like:
-                            queryWrapper.like(column, field.get(query));
-                            break;
-                        case left_like:
-                            queryWrapper.likeLeft(column, field.get(query));
-                            break;
-                        case right_like:
-                            queryWrapper.likeRight(column, field.get(query));
-                            break;
-                        case gt:
-                            queryWrapper.gt(column, field.get(query));
-                            break;
-                        case lt:
-                            queryWrapper.lt(column, field.get(query));
-                            break;
-                        case ge:
-                            queryWrapper.ge(column, field.get(query));
-                            break;
-                        case le:
-                            queryWrapper.le(column, field.get(query));
-                            break;
-                        case not_in:
-                            queryWrapper.notIn(column, field.get(query));
-                            break;
-                        case is_null:
-                            queryWrapper.isNull(column);
-                            break;
-                        case is_not_null:
-                            queryWrapper.isNotNull(column);
-                            break;
-                        case order_asc:
-                            queryWrapper.orderByAsc(column);
-                            break;
-                        case order_desc:
-                            queryWrapper.orderByDesc(column);
-                            break;
-                    }
+
                 }
             } catch (IllegalAccessException e) {
                 throw new ServiceException("Unknown error in build wrapper , field.getName():" + e);
@@ -118,6 +128,8 @@ public class QueryUtil {
 
     /**
      * 下划线转驼峰
+     * @param str 要转换的字符
+     * @return 转换后的结果
      */
     private static String lineToHump(String str) {
         str = str.toLowerCase();
@@ -132,6 +144,8 @@ public class QueryUtil {
 
     /**
      * 驼峰转下划线
+     * @param str 要转换的字符
+     * @return 转换后的结果
      */
     private static String humpToLine(String str) {
         Matcher matcher = humpPattern.matcher(str);
