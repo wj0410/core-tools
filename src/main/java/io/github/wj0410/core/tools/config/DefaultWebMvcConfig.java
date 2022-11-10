23ba0a84df6244e41bac9645e78d36075a43dd76
wangjie <89757316@qq.com>
2022-11-10 17:54:05 +0800

    ...

--- src/main/java/io/github/wj0410/core/tools/config/DefaultWebMvcConfig.java
+++ src/main/java/io/github/wj0410/core/tools/config/DefaultWebMvcConfig.java
@@ -1,9 +1,18 @@
 package io.github.wj0410.core.tools.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.github.wj0410.core.tools.interceptor.AuthSecretKeyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.TimeZone;
 
 public class DefaultWebMvcConfig implements WebMvcConfigurer {
     // 全局过滤器 验证标识头
@@ -14,5 +23,18 @@ public class DefaultWebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authSecretKeyInterceptor) // 自定义拦截器
                 .addPathPatterns("/**"); // 拦截的api路径
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(mappingJackson2HttpMessageConverter);
     }
 }
